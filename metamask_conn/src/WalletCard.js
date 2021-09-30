14c6bd1be4dbb028ab8017981abe747066e07192
Michael Couch <michaelcouch@MacBook-Pro.local>
2021-09-30 12:33:07 -0700

    address and balance showing

--- metamask_conn/src/WalletCard.js
+++ metamask_conn/src/WalletCard.js
@@ -9,13 +9,19 @@ const WalletCard = () => {
 	const [errorMessage, setErrorMessage] = useState(null);
 	const [defaultAccount, setDefaultAccount] = useState(null);
 	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
 
 	const connectWalletHandler = () => {
 		if (window.ethereum && window.ethereum.isMetaMask) {
 			console.log('MetaMask Here!');
 
 			window.ethereum.request({ method: 'eth_requestAccounts'})
-			.then(result => accountChangedHandler(result[0]));
+			.then(result => {
+				window.web3 = new Web3(window.ethereum);
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+				getAccountBalance(result[0]);
+			});
 
 		} else {
 			console.log('Need to install MetaMask');
@@ -23,17 +29,25 @@ const WalletCard = () => {
 		}
 	}
 
+	// update account, will cause component re-render
 	const accountChangedHandler = (newAccount) => {
 		console.log('Account Changed');
 		setDefaultAccount(newAccount);
 	}
+
+	const getAccountBalance = (account) => {
+		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+		.then(balance => {
+			setUserBalance(window.web3.utils.fromWei(balance));
+		});
+	};
 
 	// listen for account changes
 	window.ethereum.on('accountsChanged', accountChangedHandler);
 	
 	return (
 		<div className='walletCard'>
-			<button onClick={connectWalletHandler}>Connect Wallet</button>
+			<button onClick={connectWalletHandler}>{connButtonText}</button>
 			<div className='accountDisplay'>
 				<h3>Address: {defaultAccount}</h3>
 			</div>
